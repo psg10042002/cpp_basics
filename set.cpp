4fd71c99f9c60d85aca7d396953998aa80da7023
psg10042002 <84333523+psg10042002@users.noreply.github.com>
2022-04-15 23:13:49 +0530

    Add files via upload

---
+++ set.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namspace std;
+int main()
+{
+	std::set<int>se;
+	se.insert(1);//inserts in ascending order
+	se.insert(2);
+	se.emplace(4);
+	//begin(),end();
+	auto it=se.find(3);
+	cout<<*it;
+	auto it1=se.find(1)
+	auto it2=se.find(4)
+	auto it=se.find(1); //last+1
+	se.erase(it1,it2);//erases in range from first to last-1
+	for(set<int>::it=se.begin();it!=st.end();it++)
+
+	}
